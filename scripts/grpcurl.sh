bc887b50ca31471e7b94be13db2c8efc2addc2f5
SornchaiTheDev <sornchaithedev@gmail.com>
2025-04-15 20:10:22 +0700

    chore: update grpcurl

--- scripts/grpcurl.sh
+++ scripts/grpcurl.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-grpcurl -plaintext -d '{"name" : "C", "version": "98", "build_script" "run_script": "python3 main.py"}' \
-  localhost:8081 \
-  config.v1.ConfigService.AddLanguage
+# grpcurl -plaintext -d '{"name" : "C", "version": "98", "build_script" "run_script": "python3 main.py"}' \
+#   localhost:8081 \
+#   config.v1.ConfigService.AddLanguage
 
 # grpcurl -plaintext -d '{"id" : "python_3.11.2", "run_script": "#!/bin/bash\n\npython3 main.py"}' \
 #   localhost:8081 \
 #   config.v1.ConfigService.UpdateLanguage
 
 
-# grpcurl -plaintext -d '{"include_name" : true, "include_version" : true}' \
-#   localhost:8081 \
-#   config.v1.ConfigService.GetLanguages
+grpcurl -plaintext -d '{"include_name" : true, "include_version" : true}' \
+  localhost:8081 \
+  config.v1.ConfigService.GetLanguages
 
 
 # grpcurl -plaintext -d '{"id" : "test_1"}' \
